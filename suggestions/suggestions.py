83bf68c424cd1a233c4949ec4678befcc963d09f
SeaswimmerTheFsh <seaswimmerthefsh@gmail.com>
2023-02-27 13:20:47 -0500

    changed version number

--- suggestions/suggestions.py
+++ suggestions/suggestions.py
@@ -13,7 +13,7 @@ class Suggestions(commands.Cog):
     Adds a suggestions system.
     """
 
-    __version__ = "1.7.1"
+    __version__ = "1.0.1"
 
     def __init__(self, bot: Red):
         self.bot = bot
