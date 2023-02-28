e6905fcb588b90bc630fb1c741ddaf9224667523
SeaswimmerTheFsh <seaswimmerthefsh@gmail.com>
2023-02-28 15:45:30 -0500

    added a testing command

--- galaxy/galaxy.py
+++ galaxy/galaxy.py
@@ -10,6 +10,12 @@ class Galaxy(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    @checks.admin()
+    @commands.guild_only()
+    async def testcmd(self, ctx, role: discord.Role):
+        await ctx.send(content=f"{role}")
+
+    @commands.command()
     @commands.guild_only()
     async def userinfo(self, ctx, member: discord.Member):
         """Gives information on a specific person."""
