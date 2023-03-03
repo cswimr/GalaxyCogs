87187139dc471bd5f42476300b190abaddd0235b
SeaswimmerTheFsh <seaswimmerthefsh@gmail.com>
2023-03-03 14:11:44 -0500

    fixed an issue with server info

--- info/info.py
+++ info/info.py
@@ -75,7 +75,7 @@ class Info(commands.Cog):
                 )
             )
             if guild.icon:
-                data.set_author(name=guild.name, url=str(guild.icon_url))
+                data.set_author(name=guild.name, icon_url=str(guild.icon_url))
                 data.set_thumbnail(url=str(guild.icon_url))
             else:
                 data.set_author(name=guild.name)
